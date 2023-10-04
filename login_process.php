d9265e6197958160576031020cbcd2cc32941b64
zarex1110 <zakyimad@gmail.com>
2023-10-04 08:10:00 +0700

    Create login_process.php
    login process

---
+++ login_process.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    // Replace with your database connection logic
+    $db_username = "your_db_username";
+    $db_password = "your_db_password";
+
+    if ($username == $db_username && $password == $db_password) {
+        $_SESSION["username"] = $username;
+        header("Location: dashboard.php"); // Redirect to a dashboard or another page upon successful login
+        exit;
+    } else {
+        $error_message = "Invalid username or password.";
+    }
+}
+?>
